f6f2a07fdee1fe4f9e2f1f1fc9d0fa5ecf98f836
Suyash Matade <suyashmatade007@gmail.com>
2025-02-23 09:27:09 +0530

    Save

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -17,7 +17,7 @@ export async function POST(request) {
 
     const amount = await items.reduce(async (acc, item) => {
       const product = await Product.findById(item.product);
-      return await (acc + product.offerPrice * item.quantity);
+      return (await acc) + product.offerPrice * item.quantity;
     }, 0);
 
     await inngest.send({
